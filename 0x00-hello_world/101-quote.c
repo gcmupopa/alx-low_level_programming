b7ab19b67ede28a08af5a548055267627ab7da71
Gamuchirai Mupopa <gcmupopa@gmail.com>
2023-03-16 18:44:15 -0700

    task 0-8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This prints output to standard error
+ * Return: This returns 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
