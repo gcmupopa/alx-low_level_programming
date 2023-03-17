7651b1d7639f7ab6e40755f2c12df45901e57658
Gamuchirai Mupopa <gcmupopa@gmail.com>
2023-03-16 19:45:55 -0700

    Corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - This prints output to standard error
- * Return: This returns 1
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
-
 int main(void)
 {
-	fprintf(stderr, "\"and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
