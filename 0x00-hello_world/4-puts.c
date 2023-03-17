135028893d40e884c7c865d225a46db89f8a7800
Gamuchirai Mupopa <gcmupopa@gmail.com>
2023-03-16 17:11:15 -0700

    Task 0 - 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This prints output using puts
+ * Return: This returns 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	puts("\n");
+	return (0);
+}
