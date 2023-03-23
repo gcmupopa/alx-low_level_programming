5ce5f3ab5f660595f514841d290ac8f3fe2793c8
Gamuchirai Mupopa <gcmupopa@gmail.com>
2023-03-22 18:27:29 -0700

    Fixed bugs in task 6 - 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,7 @@
  * Return: return value of last digit
  */
 
-int print_last_digit(int)
+int print_last_digit(int n)
 {
 	int nv;
 
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,6 @@ void jack_bauer(void)
 {
 	int x = 0;
 	int a = 0, b = 0, c = 0, d = 0;
-	
 	while (x < 1440)
 	{
 		_putchar(a + '0');
@@ -17,7 +16,6 @@ void jack_bauer(void)
 		_putchar(c + '0');
 		_putchar(d + '0');
 		_putchar('\n');
-		
 		d++;
 		if (d > 9)
 		{
