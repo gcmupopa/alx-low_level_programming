4da6e51c7f35462dcae4fd4e8d68cfd3c2fef5c6
Gamuchirai Mupopa <gcmupopa@gmail.com>
2023-04-03 18:55:12 -0700

    Correction of task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_chessboard()- function that prints the chessboard
@@ -10,9 +11,9 @@ void print_chessboard(char (*a)[8])
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 8; num1++)
+	for (num1 = 0; num1 <= 8; num1++)
 	{
-		for (num2 = 0; num2 < 8; num2++)
+		for (num2 = 0; num2 <= 8; num2++)
 		{
 			_putchar(a[num1][num2]);
 		}
