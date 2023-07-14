6238690f43fb1648ba3aadfe5374f86c6adff86c
Gamuchirai Mupopa <gcmupopa@gmail.com>
2023-07-14 02:49:50 -0700

    Include _strlen function in task 1 & 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _strlen - measures length of a string
+ * @s: parameter
+ * Return: a value
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+
+/**
  * create_file()- make a file
  * @filename: name of the file to create
  * @text_content: NULL terminated string to write to thefile
@@ -9,20 +27,17 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int filed;
-	int n;
+	int filed, n;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-
-	filed = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	filed = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
 	if (filed == -1)
 	{
 		return (-1);
 	}
-
 	if (text_content != NULL)
 	{
 		n = write(filed, text_content, _strlen(text_content));
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _strlen - measures length of a string
+ * @s: parameter
+ * Return: a value
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while  (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+
+/**
  * append_text_to_file()- appends text at the end of a file
  * @filename: name of file to be appended
  * @text_content: null terminated string to add at end of file
@@ -15,13 +33,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-
 	filed = open(filename, O_WRONLY | O_APPEND);
 	if (filed == -1)
 	{
 		return (-1);
 	}
-
 	if (text_content != NULL)
 	{
 		n = write(filed, text_content, _strlen(text_content));
