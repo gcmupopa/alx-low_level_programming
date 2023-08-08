63ffb1ac372c1f7b6c7338a0d21145d6c28069cc
root <gcmupopa@gmail.com>
2023-08-07 17:50:04 -0700

    Task 0-4

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen - measures length of a string
+ * @s: parameter
+ * Return: a value
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * create_file()- make a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to thefile
+ * Return: 1 on success -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int filed, n;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	filed = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	if (filed == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		n = write(filed, text_content, _strlen(text_content));
+		if (n == -1)
+		{
+			close(filed);
+			return (-1);
+		}
+	}
+	close(filed);
+	return (1);
+}
