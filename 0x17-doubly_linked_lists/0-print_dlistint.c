fd77f9118f63bab3ca7f4e29389a1313de4cf77d
root <gcmupopa@gmail.com>
2023-08-18 04:32:54 -0700

    removed malloc in task 1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,8 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t number_nodes = 0;
-	const dlistint_t *ptr;
+	const dlistint_t *ptr = h;
 	
-	ptr = malloc(sizeof(dlistint_t));
-	ptr = h;
-	if (ptr == NULL)
-		return (1);
 	while (ptr != NULL)
 	{
 		printf("%d\n", ptr->n);
