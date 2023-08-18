0ac06350c4f0ec027296d85c008487fcfbfcf851
root <gcmupopa@gmail.com>
2023-08-18 10:53:33 -0700

    included if stmt to check if head is null in task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,18 +8,13 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *freeptr;
-	dlistint_t *node;
+	if (head == NULL)
+		return;
 
-	node = head;
-
-	while (node != NULL)
+	while (head->next)
 	{
-		freeptr = node->next;
-		free(node->prev);
-		free(node);
-		node = freeptr;
+		head = head->next;
+		free(head->prev);
 	}
 	free(head);
-	head = NULL;
 }
