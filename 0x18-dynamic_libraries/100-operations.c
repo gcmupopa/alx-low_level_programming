48e8b923d641fb1d29e72c1ae79e63edd3d82f30
root <gcmupopa@gmail.com>
2023-08-21 09:12:26 -0700

    task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add- returns the sum of 2 numbers
+ * @a: parameter
+ * @b: parameter
+ * Return: value
+ */
+
+int add(int a, int b)
+{
+        return (a + b);
+}
+
+/**
+ * sub- returns the difference of 2 numbers
+ * @a: parameter
+ * @b: parameter
+ * Return: value
+ */
+
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * mul- returns the product of 2 numbers
+ * @a: parameter
+ * @b: parameter
+ * Return: value
+ */
+
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * divy- returns the dvision of 2 numbers
+ * @a: parameter
+ * @b: parameter
+ * Return: value
+ */
+
+int divy(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod- returns the modulo of 2 numbers
+ * @a: parameter
+ * @b: parameter
+ * Return: value
+ */
+
+int mod(int a, int b)
+{
+        return (a % b);
+}
+
